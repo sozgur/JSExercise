5415c521c24f8b55e6f4bb11f85d167f7c935558
Sumeyra Ozgur <smyrozgur@gmail.com>
2021-07-27 18:04:41 -0700

    bug fiz

--- apis-tvmaze/tvmaze.test.js
+++ apis-tvmaze/tvmaze.test.js
@@ -1,6 +1,12 @@
-// decribe("Show test", function () {
-//     it("should be correct object format on searchShows()", function () {
-//         let shows = await searchShows('try');
-//         expect(Object(shows[0].keys())).toEqual(["id", "name", "summary", "image"])
-//     });
-// });
+describe("Show test", function () {
+    it("should be correct object format on searchShows()", async () => {
+        let shows = await searchShows("try");
+        console.log(shows);
+        expect(Object.keys(shows[0])).toEqual([
+            "id",
+            "name",
+            "summary",
+            "image",
+        ]);
+    });
+});
